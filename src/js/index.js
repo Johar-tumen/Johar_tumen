7c6ec511c501c7af1f975caf58bec960c181346d
Johar-tumen <58850287+Johar-tumen@users.noreply.github.com>
2020-08-07 10:52:46 +0500

    webpack_v2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log('hello webpack');
+import "../css/style.css";
+
+import Header from "./blocks/header";
+import About from "./blocks/about";
+import Products from "./blocks/products";
+
+// ПРИМЕР ИМПОРТА ВСЕГО СРАЗУ ИЗ ОДНОГО ФАЙЛА
+import * as constants from "./constants"
+constants.TERMINAL_TEXT
+
+Header();
+About();
+Products();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-};
+    entry: "./src/js/index.js",
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[hash].js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin()
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+}
